e82d4ac4a61462855faa656310522205b7f8a235
Daniel Antony <danantony06@gmail.com>
2024-12-27 23:14:05 -0400

    Updated App component and implemented

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+function Footer(){
+    return(
+        <div><div className = "footerLine"></div> 
+            <div className = "footerInfo"><h3 id = "footerText">Made with love by Daniel Antony</h3>
+              <img id = "socialMedia" src = "https://i.postimg.cc/5yKHsG5f/Screenshot-2024-12-23-130621.png" alt = "social media"></img>
+            </div>
+          </div>
+    );
+}
+
+export default Footer;
